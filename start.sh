ba22ae0c9b9db6cc5b28cf5d230ae01b7f6b0e20
Miraculous-bkm <miraclebenjamin050@gmail.com>
2024-07-02 22:46:20 -0400

    Initial commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set environment variables
+export PORT=8080
+export OPENWEATHERMAP_API_KEY=82b6601fa9a99176b3c29f354114207b
+
+# Run Gunicorn server
+gunicorn app:app --bind 0.0.0.0:$PORT --workers 4
