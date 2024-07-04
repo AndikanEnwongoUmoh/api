b97f9e4136c76b509a6866f593d0f9b24a0cffac
Miraculous-bkm <miraclebenjamin050@gmail.com>
2024-07-04 09:54:08 -0400

    Initial

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Set environment variables
-# export PORT=8080
-# export OPENWEATHERMAP_API_KEY=82b6601fa9a99176b3c29f354114207b
+# Set environment variables
+export PORT=8080
+export IPGEOLOCATION_API_KEY=5891dbc2678a42d89147271e902ac802
 
-# # Run Gunicorn server
-# gunicorn app:app --bind 0.0.0.0:$PORT --workers 4
+
+# Run Gunicorn server
+gunicorn app:app --bind 0.0.0.0:$PORT --workers 4
 
