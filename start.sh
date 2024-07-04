535875dfb3543817bfe9a7bab6c2509eba0941e2
Benjamin Miracle Kalu <141151266+Miraculous-bmk@users.noreply.github.com>
2024-07-04 18:23:48 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 # Set environment variables
 export PORT=8080
-export OPENWEATHERMAP_API_KEY=82b6601fa9a99176b3c29f354114207b
+os.environ['IPGEOLOCATION_API_KEY'] = '5891dbc2678a42d89147271e902ac802'
+os.environ['OPENWEATHERMAP_API_KEY'] = '82b6601fa9a99176b3c29f354114207b'
 
 # Run Gunicorn server
 gunicorn app:app --bind 0.0.0.0:$PORT --workers 4
